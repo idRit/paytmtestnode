fbada40c705ed59fe3d450e6ce5ded681caf5435
Ritwik Mukherjee <mritwik369@gmail.com>
2019-11-01 11:47:24 +0530

    first init

---
+++ paytm/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    MID: "EXgfFR83461690463558",
+    PAYTM_MERCHANT_KEY: "zYrd1ztZHN6%Fn5s",
+    PAYTM_FINAL_URL: "https://securegw-stage.paytm.in/order/process",
+    WEBSITE: "WEBSTAGING",
+    CHANNEL_ID: "WEB",
+    INDUSTRY_TYPE_ID: "Retail",
+    CALLBACK_URL: "http://localhost:3000/paywithpaytmresponse"
+};
